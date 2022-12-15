02850a2707685ffd345129c5e0700680200c175a
Kaique <kaiquesm07.ksm@gmail.com>
2022-12-15 16:37:15 -0300

    First commit

---
+++ Elevator/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Elevator
+//
+//  Created by Kaique Monteiro on 14/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        List {
+            
+            
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Elevator/ElevatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElevatorApp.swift
+//  Elevator
+//
+//  Created by Kaique Monteiro on 14/12/22.
+//
+
+import SwiftUI
+
+@main
+struct ElevatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
