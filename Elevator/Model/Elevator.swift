94bd8a18064ba352f379e612bf4e80c31e9ff302
Kaique Monteiro <kaique.monteiro@ab-inbev.com>
2022-12-16 11:09:36 -0300

    MOD create the first elevator

--- Elevator/Model/Elevator.swift
+++ Elevator/Model/Elevator.swift
@@ -12,3 +12,41 @@ protocol Elevator {
     var currentFloor: Int { get set }
     var targetFloors: [Int: [any Person]] { get set }
 }
+
+class SimpleElevator: Elevator {
+    var direction: Direction
+
+    var currentFloor: Int
+
+    var targetFloors: [Int : [any Person]]
+
+    init(directon: Direction = .stopped,
+         currentFloor: Int = 0,
+         targetFloors: [Int: [any Person]] = [:]) {
+        self.direction = directon
+        self.currentFloor = currentFloor
+        self.targetFloors = targetFloors
+    }
+
+    func updatePosition() {
+        switch direction {
+        case .up:
+            currentFloor += 1
+        case .down:
+            currentFloor -= 1
+        default:
+            break
+        }
+
+        if let people = targetFloors[currentFloor], !people.isEmpty {
+            targetFloors[currentFloor] = []
+        }
+    }
+
+    func load(people: [any Person]) {
+        for person in people {
+            targetFloors[person.target]?.append(person)
+        }
+    }
+
+}
